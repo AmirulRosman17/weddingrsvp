edfb4adae1963ab5c14591e9da4e0fa30d0fa525
AmirulRosman17 <150363397+AmirulRosman17@users.noreply.github.com>
2026-01-11 15:23:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+// COUNTDOWN
+const weddingDate = new Date("June 15, 2026 10:00:00").getTime();
+const countdownEl = document.getElementById("countdown");
+
+setInterval(() => {
+  const now = new Date().getTime();
+  const diff = weddingDate - now;
+
+  if (diff < 0) {
+    countdownEl.innerHTML = "Today is the big day 🎉";
+    return;
+  }
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  countdownEl.innerHTML = `${days} days to go`;
+}, 1000);
